5120094e83eea632153f5509e45d75327d8ee56b
DmitryChesnokovv <gawr03@yandex.ru>
2024-10-14 20:48:11 +0300

    Add files via upload

---
+++ module_9_6.py
@@ -0,0 +1,10 @@
+def all_variants(text):
+    for length in range(1, len(text) + 1):
+        for i in range(len(text) - length + 1):
+            yield text[i:i + length]
+
+
+a = all_variants("abc")
+
+for variant in a:
+    print(variant)
